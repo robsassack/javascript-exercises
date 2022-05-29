16af79f10d52daea7818987e41cd5846e70c385f
Rob Sassack <rsassack25@gmail.com>
2022-05-28 20:45:25 -0400

    Finished first 7 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+    for (let n=0; n < toRemove.length; n++) {
+        for (let i=0; i < arr.length; i++) {
+            if (arr[i] === toRemove[n]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    // return error if num1 or num2 are negative
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    // return error if num1 or num2 isn't a number
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
+    // set variable to hold sum
+    let sum = 0;
+    // swap the numbers if num2 < num1
+    if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    // loop through the numbers in the range
+    for (num1; num1 <= num2; num1++) {
+        // add to sum
+        sum += num1;
+    }
+    // return sum
+    return sum;
 };
 
 // Do not edit below this line
