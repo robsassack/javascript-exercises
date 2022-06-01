ca4c698a64961bbceb344a76047fb71c433cf7db
Rob Sassack <rsassack25@gmail.com>
2022-06-01 18:50:29 -0400

    Completed JavaScript exercises 8-12

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((prev, cur) =>
+    prev + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prev, cur) =>
+    prev * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let base = 1;
+  for (let i=2; i <= a; i++) {
+    base = base * i;
+  }
+  return base;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replaceAll(/[^a-z]/g, '');
+    return str === str.split('').reverse().join('');
 };
 
 // Do not edit below this line
